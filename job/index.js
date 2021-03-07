82c48c35647519f38e542141736d6a6706317015
thuocsi_datle <dat.le@thuocsi.vn>
2021-03-07 16:15:28 +0700

    fix template

--- job/index.js
+++ job/index.js
@@ -1,51 +1,53 @@
-import AWS from 'aws-sdk';
-import kue from 'kue';
-import path from 'path';
-import {
-    redis,
-} from '../config';
+import AWS from "aws-sdk";
+import kue from "kue";
+import path from "path";
+import { redis } from "../config";
 
-AWS.config.loadFromPath(path.join(__dirname, '..', '/config.json'));
+AWS.config.loadFromPath(path.join(__dirname, "..", "/config.json"));
 const ses = new AWS.SES();
 
 const ForgotPassword = "ForgotPassword";
 class JobQueue {
-    constructor() {
-        this.queue = kue.createQueue(redis);
-    }
-    async startProcess() {
-        this.queue.process(ForgotPassword, 10, async function (job, done) {
-            const {
-                name, email, link
-            } = job.data;
-            try {
-                const params = {
-                    Source: `Tapply <user1@yopmail.com>`, /* required */
-                    Destination: {
-                        ToAddresses: [email]
-                    },
-                    Template: 'ForgotPassword2', /* required */
-                    TemplateData: `{ \"name\":\"${name}\", \"link\":\"${link}\" }` /* eslint-disable-line no-useless-escape */
-                };
-                const sendMail = await ses.sendTemplatedEmail(params).promise();
-                console.log(sendMail);
-            } catch (error) {
-                console.error(error);
-            }
-            done();
-        });
-    }
+  constructor() {
+    this.queue = kue.createQueue(redis);
+  }
+  async startProcess() {
+    this.queue.process(ForgotPassword, 10, async function (job, done) {
+      const { name, email, link } = job.data;
+      try {
+        const params = {
+          Source: `Tapply <user1@yopmail.com>` /* required */,
+          Destination: {
+            ToAddresses: [email],
+          },
+          Template: "ForgotPasswordTapply" /* required */,
+          TemplateData: `{ \"name\":\"${name}\", \"link\":\"${link}\" }` /* eslint-disable-line no-useless-escape */,
+        };
+        const sendMail = await ses.sendTemplatedEmail(params).promise();
+        console.log(sendMail);
+      } catch (error) {
+        console.error(error);
+      }
+      done();
+    });
+  }
 
-    async createForgotPasswordMail(user) {
-        try {
-            const link = `http://khachhang.tapply.vn/reset-password?uid=${user.uid}&verifyToken=${user.resetToken}`;
-            await this.queue.create(ForgotPassword, { name: user.fullname, link, email: user.email }).save();
-            } catch (error) {
-                console.error(error);
-            }
+  async createForgotPasswordMail(user) {
+    try {
+      const link = `http://khachhang.tapply.vn/reset-password?uid=${user.uid}&verifyToken=${user.resetToken}`;
+      await this.queue
+        .create(ForgotPassword, {
+          name: user.fullname,
+          link,
+          email: user.email,
+        })
+        .save();
+    } catch (error) {
+      console.error(error);
     }
+  }
 }
 
-const QueueService = new JobQueue;
+const QueueService = new JobQueue();
 
 export default QueueService;
